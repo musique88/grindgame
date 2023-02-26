bde0c5b7a2cf3982a6395a247d68c29b5a8cf67c
Raphael Tremblay <raphaeltremblay01@gmail.com>
2023-02-25 21:32:28 -0500

    Added CMakeLists.txt and added basic raylib program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include <raylib.h>
 
-
-int main(int argc, const char argv[]) 
+int main(int argc, const char *argv[])
 {
-    return 0;    
+	InitWindow(720, 438, "Grind Game");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground(LIGHTGRAY);
+		DrawText("Grind Game is now started", 720 / 2, 438 / 2, 20, GREEN);
+
+		EndDrawing();
+	}
+
+	return 0;
 }
